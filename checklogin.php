f4c5dbe278dacedf43bf899b6fe2ce1498408fd0
Md Ajbullah Mansuri <124435295+MdAjbullah@users.noreply.github.com>
2023-10-22 02:39:55 +0530

    Hospital management system
    TOOLS PHP,CSS,HTML

---
+++ checklogin.php
@@ -0,0 +1,14 @@
+<?php
+// for cheaking login on the website//
+function check_login()
+{
+if(strlen($_SESSION['login'])==0)
+	{	
+		$host = $_SERVER['HTTP_HOST'];
+		$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+		$extra="../admin.php";		
+		$_SESSION["login"]="";
+		header("Location: http://$host$uri/$extra");
+	}
+}
+?>
